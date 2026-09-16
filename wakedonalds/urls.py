eedff9dc8ba78af12cf494c7dfc26d82b638f082
maaaqrabawi <maaqrabawi@my.waketech.edu>
2026-09-16 01:28:54 -0400

    Add menu_view to URL patterns

--- wakedonalds/urls.py
+++ wakedonalds/urls.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from core.views import health_check
+from core.views import health_check, menu_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
