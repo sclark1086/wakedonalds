73e03cc1e28482ab7309ecdbe1615f369caa0b55
SaiQuan <sclark1@my.waketech.edu>
2026-09-22 21:14:50 -0400

    Connect cart API URLs

---
+++ cart/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import CartView, AddToCartView, RemoveFromCartView
+
+
+urlpatterns = [
+    path("", CartView.as_view(), name="cart"),
+    path("items/", AddToCartView.as_view(), name="add-to-cart"),
+    path(
+        "items/<int:item_id>/",
+        RemoveFromCartView.as_view(),
+        name="remove-from-cart"
+    ),
+]
